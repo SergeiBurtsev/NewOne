9c921d72262f24ed561ee39e5a3b62d7952cbdd8
Sergey Burtsev <6ypka@mail.ru>
2023-04-16 21:32:37 +0300

    Initial Commit

---
+++ NewOne/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NewOne
+//
+//  Created by Serj on 16.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var nameTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController: UITextFieldDelegate {
+  
+}
